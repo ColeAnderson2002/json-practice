ca68bdf567b32df37cdb06aa8a49c01181d0f657
Cole Anderson <kbv4nd@virginia.edu>
2024-04-17 22:07:00 +0000

    lab8 jq METAR data snippet

---
+++ lab8/chacon.py
@@ -0,0 +1,8 @@
+
+#!/usr/bin/env python3
+
+import json
+
+
+with open('labs/data/schacon.repos.json', 'r') as file:
+    data = json.load(file)
--- lab8/metarjq.sh
+++ lab8/metarjq.sh
@@ -4,14 +4,6 @@
 
 curl "https://aviationweather.gov/api/data/metar?ids=KMCI&format=json&taf=false&hours=12&bbox=40%2C-90%2C45%2C-85" | jq -r .[].receiptTime | head -n 6
 
-#outputs:
-
-# 2024-04-15 02:56:17
-# 2024-04-15 01:56:17
-# 2024-04-15 00:56:22
-# 2024-04-14 23:56:20
-# 2024-04-14 22:56:12
-# 2024-04-14 21:56:19 
 
 
 
