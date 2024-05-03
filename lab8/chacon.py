ebe29956b0bea3d59a0720cc4d32465d4ef7caf9
Cole Anderson <kbv4nd@virginia.edu>
2024-05-03 18:33:41 +0000

    lab 8 python finished

--- lab8/chacon.py
+++ lab8/chacon.py
@@ -1,8 +1,14 @@
-
-#!/usr/bin/env python3
-
 import json
 
-
-with open('labs/data/schacon.repos.json', 'r') as file:
+import json
+with open('/workspace/json-practice/data/schacon.repos.json', 'r') as file: 
     data = json.load(file)
+
+with open('chacon.csv', 'w') as csv_file:
+    for fields in data[:5]: 
+        name = fields['name']
+        html_url = fields['html_url']
+        updated_at=fields['updated_at']
+        visibility = fields['visibility']
+        csv_line = f"{name},{html_url},{updated_at},{visibility}\n"
+        csv_file.write(csv_line)
